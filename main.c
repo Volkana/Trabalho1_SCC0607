3a78d980f18f0f3b29eebe746a14e653fcae9ec9
Volkana <laypeg@gmail.com>
2019-10-10 11:50:28 -0300

    Update main.c

--- main.c
+++ main.c
@@ -15,70 +15,19 @@ typedef struct{
     char campo2[100];
 }dados;
 
-int split (const char *str, char c, char ***arr){
-    int count = 1;
-    int token_len = 1;
-    int i = 0;
-    char *p;
-    char *t;
-    p = str;
-    while (*p != '\0'){
-        if (*p == c){
-            count++;
-        }
-        p++;
-    }
-
-    *arr = (char**) malloc(sizeof(char*) * count);
-    if (*arr == NULL)
-        exit(1);
-
-    p = str;
-    while (*p != '\0'){
-        if (*p == c){
-            (*arr)[i] = (char*) malloc( sizeof(char) * token_len );
-            if ((*arr)[i] == NULL){
-                    exit(1);
-            }
-            token_len = 0;
-            i++;
-        }
-        p++;
-        token_len++;
-    }
-    (*arr)[i] = (char*) malloc( sizeof(char) * token_len );
-    if ((*arr)[i] == NULL){
-            exit(1);
-    }
-    i = 0;
-    p = str;
-    t = ((*arr)[i]);
-    while (*p != '\0'){
-        if (*p != c && *p != '\0'){
-            *t = *p;
-            t++;
-        }else{
-            *t = '\0';
-            i++;
-            t = ((*arr)[i]);
-        }
-        p++;
-    }
-    return count;
-}
-
 
 Entrada ler_entrada(){
     Entrada entrada;
-    char *inicio;
-    inicio = malloc(256*sizeof(char));
+    char inicio[256];
+    char delim[] = " ";
+    char* token;
     int c = 0,i;
-    char **arr = NULL;
     printf("entrada\n");
     scanf("%s", inicio);
-    c = split(inicio, ' ', &arr);
-    for (i = 0; i < c; i++)
-        printf("string #%d: %s\n", i, arr[i]);
+    for (token = strtok(inicio, delim); token; token = strtok(NULL, delim))
+    {
+        printf("token=%s\n", token);
+    }
 
 };
 
