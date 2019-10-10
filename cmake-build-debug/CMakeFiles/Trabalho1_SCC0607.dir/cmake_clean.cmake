7bee56e8904cf363b54a4b699b9ae66043ec5c39
bariccatti <bariccatti@gmail.com>
2019-10-10 11:37:16 -0300

    Commit inicial

---
+++ cmake-build-debug/CMakeFiles/Trabalho1_SCC0607.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trabalho1_SCC0607.dir/main.c.obj"
+  "Trabalho1_SCC0607.exe"
+  "Trabalho1_SCC0607.exe.manifest"
+  "Trabalho1_SCC0607.pdb"
+  "libTrabalho1_SCC0607.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Trabalho1_SCC0607.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Trabalho1_SCC0607.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Trabalho1_SCC0607.
+# This may be replaced when dependencies are built.
